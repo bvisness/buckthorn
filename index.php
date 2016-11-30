90eeb12e46ebc519c20fc6089cb6cdcf1de4fed4
Ben Visness <bvisness@gmail.com>
2016-11-30 16:13:43 -0600

    Add database connection and credential loading

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use josegonzalez\Dotenv\Loader as EnvLoader;
+
+$loader = new EnvLoader('.env');
+$loader->parse();
+$loader->toEnv();
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,6 +17,22 @@
 
 <body>
     Hello world!
+
+<?php
+    $con = mysqli_connect($_ENV['DB_HOST'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+
+    if (!$con) {
+        echo 'Error: Unable to connect to MySQL.' . PHP_EOL;
+        echo 'Debugging errno: ' . mysqli_connect_errno() . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    echo 'Success: A proper connection to MySQL was made!' . PHP_EOL;
+    echo 'Host information: ' . mysqli_get_host_info($con) . PHP_EOL;
+
+    mysqli_close($con);
+?>
 </body>
 
 </html>
