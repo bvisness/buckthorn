827c1a1e2dd611bd05e74a2c49a3c7e041900528
Ben Visness <bvisness@gmail.com>
2016-12-07 16:24:32 -0600

    Generate insert queries based on arrays of field names

--- utilities/buckthorn.php
+++ utilities/buckthorn.php
@@ -2,31 +2,47 @@
 
 require_once 'mysql.php';
 
-// The string syntax below is called Nowdoc. It's super gross but it does let
-// you span multiple lines.
+$insert_observation_fields = [
+    't_id',
+    'o_date',
+    'o_latitude',
+    'o_longitude',
+    'o_quadrantsize',
+    'o_numstems',
+    'o_foliar',
+    'o_circumference',
+    'o_photos',
+];
 
-$query_insert_observation = <<<'EOD'
-INSERT
-INTO observation ( t_id ,  o_date ,  o_latitude ,  o_longitude ,  o_quadrantsize ,  o_numstems ,  o_foliar ,  o_circumference ,  o_photos )
-VALUES           (%t_id%, %o_date%, %o_latitude%, %o_longitude%, %o_quadrantsize%, %o_numstems%, %o_foliar%, %o_circumference%, %o_photos%)
-EOD;
+$insert_bio_count_fields = [
+    'o_id',
+    'bc_count',
+    'bc_is_buckthorn',
+];
 
-$query_insert_bio_count = <<<'EOD'
-INSERT
-INTO bio_count ( o_id ,  bc_count ,  bc_is_buckthorn )
-VALUES         (%o_id%, %bc_count%, %bc_is_buckthorn%)
-EOD;
+$insert_competition_fields = [
+    'o_id',
+    'c_dbh_buckthorn',
+    'c_dbh_neighbor_b',
+    'c_dbh_neighbor_nb',
+    'c_distance_b',
+    'c_distance_nb',
+    'c_notes',
+];
 
-$query_insert_competition = <<<'EOD'
-INSERT
-INTO competition ( o_id ,  c_dbh_buckthorn ,  c_dbh_neighbor_b ,  c_dbh_neighbor_nb ,  c_distance_b ,  c_distance_nb ,  c_notes )
-VALUES           (%o_id%, %c_dbh_buckthorn%, %c_dbh_neighbor_b%, %c_dbh_neighbor_nb%, %c_distance_b%, %c_distance_nb%, %c_notes%)
-EOD;
+$insert_notes_fields = [
+    'o_id',
+    'n_habitat',
+    'n_general',
+    'n_biodiversity',
+    'n_competition',
+];
 
-$query_insert_notes = <<<'EOD'
-INSERT
-INTO notes ( o_id ,  n_habitat ,  n_general ,  n_biodiversity ,  n_competition )
-VALUES     (%o_id%, %n_habitat%, %n_general%, %n_biodiversity%, %n_competition%)
-EOD;
+function generate_insert_query($table, $fields)
+{
+    $percented_fields = array_map(function ($field) {
+        return '%' . $field . '%';
+    }, $fields);
 
-// Functions for these queries will go here.
+    return "INSERT INTO $table (" . implode($fields, ', ') . ') VALUES (' . implode($percented_fields, ', ') . ')';
+}
