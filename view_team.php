bc42138bd300ec9e566c306763fa0c34fff4fb94
Ben Visness <bvisness@gmail.com>
2016-12-09 16:45:04 -0600

    Add scaffolding for View Team page

---
+++ view_team.php
@@ -0,0 +1,16 @@
+<?php
+    require_once 'utilities/output_helpers.php';
+?>
+<?php
+    $header_options['title'] = 'View Team';
+    include 'templates/header.php';
+?>
+
+<?php /* ------------------- PAGE CONTENT BEGINS HERE ------------------- */ ?>
+
+    <p>Hi, Tyler! This is the view team page.</p>
+
+<?php /* ------------------- PAGE CONTENT ENDS HERE ------------------- */ ?>
+<?php
+    include 'templates/footer.php';
+?>
