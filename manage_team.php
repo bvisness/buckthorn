0d979d326e197faa7811586540bbc66a28fa38ac
JACOB-PC\Jacob <zatari88@gmail.com>
2016-12-12 14:58:01 -0600

    manage_team.php : page now contains content about active team members and puts them in a table

--- manage_team.php
+++ manage_team.php
@@ -9,6 +9,40 @@
 
 <?php /* ------------------- PAGE CONTENT BEGINS HERE ------------------- */ ?>
 
+	<?php
+        $current_team = query('SELECT * FROM researcher natural join membership where t_id = %t_id% and (end > NOW() or end is null)', [
+            't_id' => $_GET['id'],
+        ]);
+    ?>
+	<?php 
+		$team_name = query_first('SELECT * FROM team WHERE t_id = %t_id%', [
+            't_id' => $_GET['id'],
+        ]);
+	?>
+	<h1><?php echo $team_name['t_name']?></h1>
+	
+	<table>
+        <thead>
+            <tr>
+				<th>Team member ID</th>
+                <th>Team member name</th>
+                <th>Begin Date</th>
+				<th>End Date</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($current_team as $member): ?>
+                <tr>
+                    <td><?php echo $member['r_id'] ?></td>
+                    <td><?php echo $member['r_name'] ?></td>
+                    <td><?php echo $member['begin'] ?></td>
+                    <td><?php echo $member['end'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
 <?php /* ------------------- PAGE CONTENT ENDS HERE ------------------- */ ?>
 <?php
     include 'templates/footer.php';
