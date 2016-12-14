4fb467cf84bfc8c177280af233174ff2d1aee767
Tyler Miller <tym98445@bethel.edu>
2016-12-14 14:10:19 -0600

    Added permissions for create membership, researcher, and team

--- create_membership.php
+++ create_membership.php
@@ -2,19 +2,24 @@
 
     require_once 'utilities/mysql.php';
     require_once 'utilities/output_helpers.php';
-	$existing_membership = query("SELECT * FROM membership
-													WHERE t_id = '%t_id%'
-													AND r_id = '%r_id%'
-													AND end is null",[
-													'r_id' => $_GET['r_id'],
-													't_id' => $_GET['t_id'],
-													]);
-	if(empty($existing_membership)){
-		$new_teammate_query = query('INSERT INTO membership (r_id, t_id, begin)
-														VALUES (%r_id%,%t_id%, NOW())',[
+    require_once 'utilities/user.php';
+    if (is_admin()):
+		$existing_membership = query("SELECT * FROM membership
+														WHERE t_id = '%t_id%'
+														AND r_id = '%r_id%'
+														AND end is null",[
 														'r_id' => $_GET['r_id'],
 														't_id' => $_GET['t_id'],
 														]);
-	}
-	$manage_team_url = url("manage_team.php?id=$_GET[t_id]");
-	redirect($manage_team_url);
+		if(empty($existing_membership)){
+			$new_teammate_query = query('INSERT INTO membership (r_id, t_id, begin)
+															VALUES (%r_id%,%t_id%, NOW())',[
+															'r_id' => $_GET['r_id'],
+															't_id' => $_GET['t_id'],
+															]);
+		}
+		$manage_team_url = url("manage_team.php?id=$_GET[t_id]");
+		redirect($manage_team_url);
+	else:
+		redirect('/');
+	endif;
--- create_team.php
+++ create_team.php
@@ -2,14 +2,17 @@
 
     require_once 'utilities/mysql.php';
     require_once 'utilities/output_helpers.php';
-	$existing_team = query("SELECT * FROM team
-										WHERE t_name = '%t_name%'",[
-										't_name' => $_POST['team_name'],
-										]);
-	if(empty($existing_team)){
-		$new_team_query = query("INSERT INTO team (t_name)
-														VALUES ('%t_name%')",[
-														't_name' => $_POST['team_name'],
-														]);
-	}
+    require_once 'utilities/user.php';
+    if (is_admin()):
+		$existing_team = query("SELECT * FROM team
+											WHERE t_name = '%t_name%'",[
+											't_name' => $_POST['team_name'],
+											]);
+		if(empty($existing_team)){
+			$new_team_query = query("INSERT INTO team (t_name)
+															VALUES ('%t_name%')",[
+															't_name' => $_POST['team_name'],
+															]);
+		}
+	endif;
 	redirect(url("/list_teams.php"));
